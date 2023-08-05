6ff97b24f9a7c7657ab06f4717074d854dbf2dac
Daniel Neto <daniel@danielneto.dev.br>
2023-08-05 16:45:16 -0300

    6.1. Defining an Enum

---
+++ enums-and-pattern-matching/defining-an-enum/src/main.rs
@@ -0,0 +1,45 @@
+use std::any::Any;
+
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+// struct IpAddr {
+//     kind: IpAddrKind,
+//     address: String,
+// }
+
+enum IpAddr {
+    V4(String),
+    V6(String),
+}
+
+fn main() {
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    route(IpAddrKind::V4);
+    route(IpAddrKind::V6);
+
+    // let home = IpAddr {
+    //     kind: IpAddrKind::V4,
+    //     address: String::from("127.0.0.1"),
+    // };
+    //
+    // let loopback = IpAddr {
+    //     kind: IpAddrKind::V6,
+    //     address: String::from("::1"),
+    // };
+
+    let home = IpAddr::V4(String::from("127.0.0.1"));
+
+    let loopback = IpAddr::V6(String::from("::1"));
+
+    let some_number = Some(5);
+    let some_char = Some('e');
+
+    let absent_number: Option<i32> = None;
+}
+
+fn route(ip_kind: IpAddrKind) {}
